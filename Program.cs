c3bb280be8ca0e09e096f8342c0283a1d29faa4a
hajimat999 <hajimatfa@code.edu.az>
2022-04-12 01:06:56 +0400

    Riri

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NewHomework999
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deyer Daxil Edin");
+            string b = Console.ReadLine();
+           
+
+            while (true)
+            {
+
+
+                if (b.Length >= 8)
+                {
+
+                    
+                    break;
+                }
+                else
+                {
+
+                    Console.WriteLine("Yeniden daxil edin");
+                    b = Console.ReadLine();
+                }
+            }
+
+
+            User user1 = new User(b);
+
+            Console.WriteLine("Password deyerini daxil edin");
+            user1.Password = Console.ReadLine();
+            Console.WriteLine("Istifadeci passwordu: " + user1.Password);
+            
+            
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewHomework999
+{
+    class User
+    {
+        public string UserName;
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+
+
+            }
+            set
+            {
+                
+                int countupper = 0;
+                int countlower = 0;
+                int countdigit = 0;
+                while (true)
+                {
+
+                    if (value.Length >= 8)
+                    {
+                        foreach (char item in value)
+                        {
+                            if (char.IsUpper(item))
+                            {
+                                countupper++;
+                                
+
+                            }
+                            else if(char.IsLower(item))
+                            {
+                                countlower++;
+                                
+
+                            }
+                            else if(char.IsDigit(item))
+                            {
+                                countdigit++;
+                                
+
+                            }
+
+                        }
+                        if (countdigit != 0 && countlower !=0 && countupper !=0)
+                        {
+
+                            _password = value;
+                            
+
+                            break;
+
+
+                        }
+                        if (countdigit == 0 || countlower == 0 || countupper == 0)
+                        {
+                            Console.WriteLine("Duzgun deyer daxil et");
+                            value = Console.ReadLine();
+                            
+                        }
+
+
+
+
+
+                        
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yeniden daxil et");
+                        value = Console.ReadLine();
+                        
+                        
+                    }
+
+
+
+
+                }   
+
+                    
+                
+
+
+
+            }
+        }
+        public User(string username)
+        {
+            UserName = username;
+            Console.WriteLine("Istifadeci Username: " + UserName);
+        }
+
+
+
+
+
+
+
+       
+    }
+}
